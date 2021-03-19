0fc4b195f6a5101cb7571fdbfb5581bff5fdbdf1
Sashen Naidoo <mail@sashen.co.za>
2021-03-19 21:24:13 +0200

    Account page ready for backend

--- src/components/RetrieveQRCode.jsx
+++ src/components/RetrieveQRCode.jsx
@@ -4,7 +4,11 @@ import "../App.css";
 export default function RetrieveQRCode(props) {
   return (
     <div>
-      QR Screen
+     <div>
+        <p>Please enter your phone number below:</p>
+               <input placeholder="0833571821" className="capitalize" maxLength={10}></input>
+               <br></br><br></br><button className="paymentButton">Retrieve QR Code</button>
+    </div>
     </div>
   );
 }
--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -4,7 +4,9 @@ import "../App.css";
 export default function SignUpPage(props) {
   return (
     <div>
-     SignUpPage
+        <p>Please enter your phone number below:</p>
+               <input placeholder="0833571821" className="capitalize" maxLength={10}></input>
+               <br></br><br></br><button className="paymentButton">Sign up</button>
     </div>
   );
 }
