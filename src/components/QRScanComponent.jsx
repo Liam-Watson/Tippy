ab1baed2b5ab375d598e5267f62bee0a9dd5894b
Liam Watson <liam@watson.org.za>
2021-03-19 16:10:17 +0200

    QR scanning v1

--- src/components/QRScanComponent.jsx
+++ src/components/QRScanComponent.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 
 export default function QRScanComponent(props) {
+  //Essentially Yoink from with a few changes :) : https://codesandbox.io/s/qr-code-scanner-ilrm9?file=/index.html
+  useEffect(() => {
+    const qrcode = window.qrcode;
+
+    const video = document.createElement("video");
+    const canvasElement = document.getElementById("qr-canvas");
+    const canvas = canvasElement.getContext("2d");
+
+    const qrResult = document.getElementById("qr-result");
+    const outputData = document.getElementById("outputData");
+    let scanning = false;
+
+    qrcode.callback = (res) => {
+      if (res) {
+        console.log(res);
+        outputData.innerText = res;
+        scanning = false;
+        video.srcObject.getTracks().forEach((track) => {
+          track.stop();
+        });
+
+        qrResult.hidden = false;
+        canvasElement.hidden = true;
+      }
+    };
+
+    const onclick = () => {
+      navigator.mediaDevices
+        .getUserMedia({ video: { facingMode: "environment" } })
+        .then(function (stream) {
+          scanning = true;
+          qrResult.hidden = true;
+          canvasElement.hidden = false;
+          video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+          video.srcObject = stream;
+          video.play();
+          tick();
+          scan();
+        });
+    };
+    onclick();
+    function tick() {
+      //TODO: Fix video size @Sashen
+      canvasElement.height = video.videoHeight; 
+      canvasElement.width = video.videoWidth;
+      canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+      scanning && requestAnimationFrame(tick);
+    }
+
+    function scan() {
+      try {
+        qrcode.decode();
+      } catch (e) {
+        setTimeout(scan, 300);
+      }
+    }
+  }, []);
   return (
-    <div className="QRArea">
-QR Scan Component (to be done)<br></br><br></br>(QRScanComponent.jsx)
+    <div id="container">
+      <h1>QR Code Scanner</h1>
+
+      <a id="btn-scan-qr">
+      </a>
+
+      <canvas hidden="" id="qr-canvas"></canvas>
+
+      <div id="qr-result" hidden="">
+        <b>Data:</b> <span id="outputData"></span>
+      </div>
     </div>
   );
 }
