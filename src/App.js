4f1e611f950eab9b0c7737d72048139361bebf6c
Sashen Naidoo <mail@sashen.co.za>
2021-03-19 14:04:36 +0200

    css changes

--- src/App.js
+++ src/App.js
@@ -5,31 +5,65 @@ import { Home, Payment, AccountBalance } from "@material-ui/icons";
 import HomePage from "./components/HomePage";
 import PaymentPage from "./components/PaymentPage";
 import ClientPage from "./components/ClientPage";
+import HomeMoreInfo from "./components/HomeMoreInfo";
+import PaymentAmount from "./components/paymentAmount";
+import PaymentConfirmation from "./components/paymentConfirmation";
 
 window.onload = window.onresize = function () {
-  var header= document.getElementById("Header");
-  var mainContentArea= document.getElementById("primaryContentAreaDiv");
-  var footer= document.getElementById("Footer");
+  var header = document.getElementById("Header");
+  var mainContentArea = document.getElementById("primaryContentAreaDiv");
+  var footer = document.getElementById("Footer");
   var height = window.innerHeight;
-  var usableArea=height-(header.clientHeight+footer.clientHeight)
-  mainContentArea.style.height=usableArea+"px";
-  }
+  var usableArea = height - (header.clientHeight + footer.clientHeight);
+  mainContentArea.style.height = usableArea + "px";
+};
 
 function App() {
   const [value, setValue] = useState(0);
   //Choose which page to display. Default is home and initial is home
+  const infoCallBack = () => {
+    setValue(3);
+  }
+  const paymentCallBack = () => {
+    setValue(4);
+  }
+
+  const paymentAmtCallBack = () => {
+    setValue(5);
+  }
+
   const screenToShow = () => {
     switch (value) {
       case 0:
-        return <Home />;
+        return <HomePage infoCallBackFn={infoCallBack} />;
       case 1:
-        return <PaymentPage />;
+        return <PaymentPage paymentCallBackFn={paymentCallBack}/>;
       case 2:
         return <ClientPage />;
+      case 3:
+        return <HomeMoreInfo />;
+        case 4:
+        return <PaymentAmount paymentAmtCallBackFn={paymentAmtCallBack}/>;
+        case 5:
+          return <PaymentConfirmation />;
       default:
-        return <Home />;
+        return <HomePage infoCallBackFn={infoCallBack} />;
     }
   };
+
+  function getValue(){
+  switch(value){
+    case 3: 
+    return 0;
+    case 4: 
+    return 1;
+    case 5:
+    return 1;
+    default:
+      return value;
+  
+    }
+  }
 
   return (
     <div className="App">
@@ -46,7 +80,7 @@ function App() {
 
       {/* Footer code: start */}
       <div id="Footer">
-        <BottomNavigation value={value} showLabels className="navbar">
+        <BottomNavigation value={getValue()} showLabels className="navbar">
           <BottomNavigationAction
             label="Home"
             icon={<Home />}
