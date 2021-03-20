052de146f6503061c2ec1bb2e2d623ba1c633623
Liam Watson <liam@watson.org.za>
2021-03-20 16:35:25 +0200

    Fixed bug where QR code download is broken

--- src/components/QrGenerator.jsx
+++ src/components/QrGenerator.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import GetAppIcon from '@material-ui/icons/GetApp';
+import React from "react";
+import GetAppIcon from "@material-ui/icons/GetApp";
 
-export default function QRDisplay(props){
-    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+export default function QRDisplay(props) {
+  <a href="#">
+    <i class="fab fa-linkedin-in"></i>
+  </a>;
 
-    function shareFunc(){
-document.getElementById("share").style.visibility="hidden";
-document.getElementById("shareButtons").style.visibility="visible";
-    }
+  function shareFunc() {
+    document.getElementById("share").style.visibility = "hidden";
+    document.getElementById("shareButtons").style.visibility = "visible";
+  }
 
-    
-
-    return <div>
-       
-<img alt="" className="qrBorderRound"src={`https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=${props.number}`}/>
-<div >
-  <a  href={"https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=${props.number}"} download="myQRCode">
- <div className="shareButton">
-<GetAppIcon ></GetAppIcon>
-</div>
-</a>
-
-</div>
-</div>
-
+  return (
+    <div>
+      <img
+        alt=""
+        className="qrBorderRound"
+        src={`https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=${props.number}`}
+      />
+      <div>
+        <a
+          href={`https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=${props.number}`}
+          download="myQRCode"
+        >
+          <div className="shareButton">
+            <GetAppIcon></GetAppIcon>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 }
